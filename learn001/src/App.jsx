f1f24ba5015d0be9695132d8f69d33b6e4315999
noreenjahan <noreenjahan10@gmail.com>
2026-03-29 21:27:28 +0500

    first commit

---
+++ learn001/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent'>
+    <h1>app</h1>
+    <p>Lorem ipsum dolor sit amet consectetur,
+       adipisicing elit. Ducimus ad saepe ipsam
+       dolor veritatis! Fugiat, possimus quo iusto 
+       dolore vero, consequatur deleniti, est maiores 
+       magnam incidunt dicta repellendus natus illum.</p>
+     </div>
+  )
+}
+
+export default App
